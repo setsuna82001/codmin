593a7c23480a28aa46405c27c2cebdffac082574
setsuna82001 <setsuna82001@yahoo.co.jp>
2017-09-26 18:56:30 +0900

    modify: dmm_content detail

--- config/initializers/modules/dmm_content.rb
+++ config/initializers/modules/dmm_content.rb
@@ -1,12 +1,20 @@
 module DMMContent
+  #=====================================
+  # DMMContent::ClassMethods
+  #=====================================
   class << self
+    # TODO cache
+
+    #===================================
+    # DMMContent::search
+    #===================================
     def search str
       # access to top page
       agent = Mechanize.new
       page  = agent.get 'http://www.dmm.com/'
       # get search result page
       page  = page.form_with(action: '/search/'){|form|
-        form['searchstr']= str
+        form['searchstr']= str.to_s
         form['category']= 'comic'
       }.submit
 
@@ -14,20 +22,22 @@ module DMMContent
       page.search('ul#fn-list/li').map &:to_search_data
     end
 
-
+    #===================================
+    # DMMContent::detail
+    #===================================
     def detail url
-      url = 'https://book.dmm.com/detail/b508akaeb00004/' # TODO DELETE
       # access to detail page
       agent = Mechanize.new
-      page  = agent.get url
+      page  = agent.get url.to_s
 
       # parsing
       authors = page.search('dl.m-boxDetailProductInfoMainList[1]//a').map(&:text >> :chomp)
       tags    = page.search('dl.m-boxDetailProductInfo__list[6]//a').map(&:text >> :chomp)
 
+      # return
       compact %i(authors tags)
-
     end
 
   end
+  #=====================================
 end
