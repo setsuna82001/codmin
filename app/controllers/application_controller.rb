3eefbffddfbe240b084ad3d362075da76e1763a1
setsuna82001 <setsuna82001@yahoo.co.jp>
2017-10-04 16:58:43 +0900

    modify: controller test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :require_login
+  before_action :require_login unless Rails::env.test?
 
   private
   def not_authenticated
